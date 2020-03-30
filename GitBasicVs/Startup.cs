7f91ac9f7e955bed7344648ef6ac2ead7e2efdec
Rafael Chamon Garcia <rchamon@outlook.es>
2020-03-30 12:38:33 +0200

    tercer commit

--- GitBasicVs/Startup.cs
+++ GitBasicVs/Startup.cs
@@ -24,6 +24,7 @@ namespace GitBasicVs
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            int a = 10;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
